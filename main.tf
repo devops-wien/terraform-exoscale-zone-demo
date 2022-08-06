4cd3406c24a040c5bf65113768f67a6dd4c98232
devops-wien-user <devops.wien@gmail.com>
2022-08-06 05:00:16 +0200

    EXO-4: [Change] update main.tf - add sks_nodepool, deployment modules

--- main.tf
+++ main.tf
@@ -2,7 +2,31 @@ module "sks" {
   source  = "devops-wien/sks/exoscale"
   version = "0.0.1"
 
-  key = "${var.sks_key}"
-  secret = "${var.sks_secret}"
+  key    = var.sks_key
+  secret = var.sks_secret
 }
 
+module "sks_nodepool" {
+  source                 = "devops-wien/sks_nodepool/exoscale"
+  version                = "0.0.2"
+  client_certificate     = module.sks.client_certificate
+  client_key             = module.sks.client_key
+  cluster_ca_certificate = module.sks.cluster_ca_certificate
+  cluster_id             = module.sks.id
+  host                   = module.sks.host
+  key                    = var.sks_key
+  secret                 = var.sks_secret
+  security_group_ids     = [module.sks.sks-security-group-id]
+  email                  = var.email
+}
+
+module "deployment" {
+  source                 = "devops-wien/deployment/kubernetes"
+  client_certificate     = module.sks.client_certificate
+  client_key             = module.sks.client_key
+  cluster_ca_certificate = module.sks.cluster_ca_certificate
+  host                   = module.sks.host
+  dns_names              = var.dns_names
+  image                  = var.image
+  target_port            = var.target_port
+}
--- variables.tf
+++ variables.tf
@@ -7,3 +7,23 @@ variable "sks_secret" {
   description = "Exoscale account API secret"
   type        = string
 }
+
+variable "dns_names" {
+  type    = list(string)
+  default = ["example.com"]
+}
+
+variable "target_port" {
+  type    = number
+  default = 80
+}
+
+variable "image" {
+  type    = string
+  default = "yeasy/simple-web"
+}
+
+variable "email" {
+  type    = string
+  default = "exmaple@example.org"
+}

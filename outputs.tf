723e5ee6da7c6b8f073cd0353542a5b840426d11
devops-wien-user <devops-wien@gmail.com>
2022-08-09 00:54:55 +0200

    EXO-4: [NEW] update outputs.tf - add req vars

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,30 @@ output "url" {
 output "cluster_url" {
   value = local.cluster_url
 }
+
+output "cloudflare_api_token" {
+  value = local.cluster_url
+}
+
+output "cloudflare_zone_id" {
+  value = var.cloudflare_zone_id
+}
+output "nlb_ip_address" {
+  value = module.sks_nodepool.nlb_ip_address
+}
+
+output "client_certificate" {
+  value = module.sks.client_certificate
+}
+
+output "client_key" {
+  value = module.sks.client_key
+}
+
+output "cluster_ca_certificate" {
+  value = module.sks.cluster_ca_certificate
+}
+
+output "host" {
+  value = module.sks.host
+}

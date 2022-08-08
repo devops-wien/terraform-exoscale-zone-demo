17f179d72a07b761c6e17b36a4cc56216b5745e8
devops-wien-user <devops-wien@gmail.com>
2022-08-09 01:30:38 +0200

    Revert "EXO-4: [NEW] update provider.tf - add kubernetes"
    This reverts commit 676981b0aed78cd5de9bc2db3887e946d2380115.

--- provider.tf
+++ provider.tf
@@ -1,11 +1,3 @@
 provider "cloudflare" {
   api_token = var.cloudflare_api_token
 }
-
-# remove (only 4 migrate)
-provider "kubernetes" {
-  host                   = module.sks.host
-  client_certificate     = base64decode(module.sks.client_certificate)
-  client_key             = base64decode(module.sks.client_key)
-  cluster_ca_certificate = base64decode(module.sks.cluster_ca_certificate)
-}

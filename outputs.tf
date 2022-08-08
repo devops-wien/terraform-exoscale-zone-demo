f0297f5a12a9705acfd578f03d6c7f6a6f726132
devops-wien-user <devops-wien@gmail.com>
2022-08-09 00:57:38 +0200

    EXO-4: [NEW] update outputs.tf - add req vars

--- outputs.tf
+++ outputs.tf
@@ -22,17 +22,28 @@ output "nlb_ip_address" {
 }
 
 output "client_certificate" {
-  value = module.sks.client_certificate
+  value     = module.sks.client_certificate
+  sensitive = true
 }
 
 output "client_key" {
-  value = module.sks.client_key
+  value     = module.sks.client_key
+  sensitive = true
 }
 
 output "cluster_ca_certificate" {
-  value = module.sks.cluster_ca_certificate
+  value     = module.sks.cluster_ca_certificate
+  sensitive = true
 }
 
 output "host" {
   value = module.sks.host
+}
+
+output "domain" {
+  value = var.domain
+}
+
+output "env_name" {
+  value = var.env_name
 }

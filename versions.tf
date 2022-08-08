4609677442bfc080a60690faca4dc72bb15323b3
devops-wien-user <devops-wien@gmail.com>
2022-08-09 01:30:38 +0200

    Revert "EXO-4: [NEW] update versions.tf - add kubernetes"
    This reverts commit f9a4b2a2f836359f7db72b0f22b9f697bcb68944.

--- versions.tf
+++ versions.tf
@@ -4,10 +4,6 @@ terraform {
       source  = "cloudflare/cloudflare"
       version = "~> 3.20.0"
     }
-    kubernetes = {
-      source = "hashicorp/kubernetes"
-      version = "2.12.1"
-    }
   }
   required_version = ">= 1.2.6"
 }
